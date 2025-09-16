7f116d586554541d81e728c6abb7de313aa77770
DominicDev <s0l0estudi4r@gmail.com>
2025-09-16 11:20:10 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = ' FREE FIRE';
+export const nameSongs = [
+	"CHRISTMAS THEME", /*1*/
+	"FREE FIRE BATTLEGROUND", /*2*/
+	"VALE VALE", /*3*/
+	"OB 14 - THEME", /*4*/
+	"OB 17 - THEME", /*5*/
+	"OB 7 - THEME", /*6*/
+	"SPOOKY NIGHT MUSIC", /*7*/
+	"WINTERLAND 2019", /*8*/
+	"BEACH PARTY", /*9*/
+	"RAMPAGE 2.0 ", /*10*/
+	"3RD ANNIVERSARY", /*11*/
+	"BOOYAH DAY", /*12*/
+	"CONTINENTAL SERIES", /*13*/
+	"CRONO UPDATE", /*14*/
+	"THE COBRA ", /*15*/
+	"AOT - THEME", /*16*/
+	"OB27", /*17*/
+	"MONEY HEIST NEW LOBBY", /*18*/
+	"WORLD SERIES", /*19*/
+	"FREE FIRE OB 28", /*20*/
+]
